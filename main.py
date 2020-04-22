f0df6b4595d2faaba372c1431ca9e1bd4768e70e
splineguy <drscottfranklin@gmail.com>
2020-04-21 21:22:17 -0500

    Change Background and include set fill for window

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 # Initialize the pygame
-pygame.init()
+pygame.init()  # ALWAYS INCLUDE IN PYGAME
 
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
@@ -9,9 +9,13 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # RGB - Red, Green Blue
+    screen.fill((255, 0, 0))
+    pygame.display.update()  # ALWAYS INCLUDE IN PYGAME
