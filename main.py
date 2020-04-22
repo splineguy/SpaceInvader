35589d7f2831f999afd78adda48bed1ab115ad67
splineguy <drscottfranklin@gmail.com>
2020-04-21 21:34:57 -0500

    Add player image to screen

--- main.py
+++ main.py
@@ -5,17 +5,29 @@ pygame.init()  # ALWAYS INCLUDE IN PYGAME
 
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Window Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
 
 running = True
 while running:
+    # RGB - Red, Green, Blue
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RGB - Red, Green Blue
-    screen.fill((255, 0, 0))
+    player()
+
     pygame.display.update()  # ALWAYS INCLUDE IN PYGAME
